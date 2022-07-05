84ef8ed3604585ba9da5105c181f2f538e64be47
IsabellaTrajano <bellrdapct@gmail.com>
2022-07-05 17:52:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let Comedy = [{
+    Title: 'Cheaper by the Dozen'
+}, 
+{
+    Director: 'Shawn Levy'
+}, 
+{
+    Year: 2003
+}]
+console.log(Comedy)
+
+let Action = [{
+    Title: 'In Time'
+}, 
+{
+    Director: 'Andrew Niccol'
+}, 
+{
+    Year: 2011
+}]
+console.log(Action)
+
+let Fantasy = [{
+    Title: 'Fantastic Beasts and Where to Find Them'
+}, 
+{
+    Director: 'David Yates'
+}, 
+{
+    Year: 2016
+}]
+console.log(Fantasy)
+
